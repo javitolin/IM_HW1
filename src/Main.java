dbbb9d1c103b84f45a79819b3a2d1176a2f82191
Javier Dorfsman <javitolin@gmail.com>
2015-04-10 12:30:14 +0300

    HW1 first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import aima.core.logic.propositional.parsing.PLParser;
+import aima.core.search.framework.Problem;
+
+
+
+public class Main {
+	private PLParser parser = new PLParser();
+	public static void main(String[] args) {
+		
+	}
+
+}
