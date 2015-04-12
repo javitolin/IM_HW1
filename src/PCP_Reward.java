b217fe44cd35fd2629d0bc46cc8ca9f266b7459e
Javier Dorfsman <javitolin@gmail.com>
2015-04-12 14:20:50 +0300

    Fixed something small

--- src/PCP_Reward.java
+++ src/PCP_Reward.java
@@ -5,7 +5,8 @@ public class PCP_Reward implements RewardFunction<PCP_State>{
 
 	@Override
 	public double reward(PCP_State state) {
-		if(state.isLastPatientSurvived() || state.getDiagnose() == 'f') 
+		//if(state.isLastPatientSurvived() || state.getDiagnose() == 'f') I don't think this is right.. 
+		if(state.isLastPatientSurvived())
 			return 1;
 		return 0;
 	}
